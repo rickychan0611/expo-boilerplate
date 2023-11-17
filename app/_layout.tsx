a477ea8acc5b92a6ddcebbce4b848ab0a936176f
Ricky Chan <33400559+rickychan0611@users.noreply.github.com>
2023-11-17 13:40:40 -0800

    add web support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux'
 import { store } from '@/src/redux/store'
 import tw from "twrnc"
+import { isWeb } from '@/src/utils/checkPlatform';
 
 export default function AppLayout() {
 
@@ -12,33 +13,35 @@ export default function AppLayout() {
 
   return (
     <Provider store={store}>
-      <View style={tw`h-full w-full pt-${insets.top || 1}px`}>
-        <Tabs screenOptions={{ headerShown: false }}>
-          <Tabs.Screen
-            name="index"
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Icon_House fill={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="home"
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Icon_House fill={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="about"
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Icon_Gear fill={color} />
-              ),
-            }}
-          />
-        </Tabs>
+      <View style={tw`flex-1 pt-${insets.top || 1}px justify-center items-center ${isWeb ? "bg-stone-200" : "bg-white"}`}>
+        <View style={tw`h-full w-full max-w-[1000px] bg-white`}>
+          <Tabs screenOptions={{ headerShown: false }}>
+            <Tabs.Screen
+              name="index"
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon_House fill={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="home"
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon_House fill={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="about"
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon_Gear fill={color} />
+                ),
+              }}
+            />
+          </Tabs>
+        </View>
       </View>
     </Provider>
   );
